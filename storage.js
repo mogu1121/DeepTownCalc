78feb5a1bb45562cfafbb0807cf5434d2ea1fb46
mogu1121 <gwmzyqmyjgd@gmail.com>
2021-03-13 20:04:49 +0800

    localStorage

---
+++ storage.js
@@ -0,0 +1,40 @@
+function get_current_options_index(ele) {
+  for (let i = 0, len = ele.options.length; i < len; i++) {
+    if (ele.options[i].selected) {
+      return i;
+    }
+  }
+}
+
+function save(item, input) {
+  const index = get_current_options_index(item);
+  const quantity = Number.parseInt(input.value);
+
+  const data = { index, quantity };
+
+  localStorage.setItem("data", JSON.stringify(data));
+}
+
+function restore(item, input) {
+  const get = localStorage.getItem("data");
+
+  if (get) {
+    const { index, quantity } = JSON.parse(get);
+
+    item.options[index].selected = true;
+    input.value = quantity;
+  }
+}
+
+function load() {
+  const item = document.querySelector("select");
+  const input = document.querySelector(".quantity > input");
+
+  item.addEventListener("input", save.bind(window, item, input), false);
+  input.addEventListener("input", save.bind(window, item, input), false);
+
+  restore(item, input);
+  window.removeEventListener("load", load);
+}
+
+window.addEventListener("load", load, false);
